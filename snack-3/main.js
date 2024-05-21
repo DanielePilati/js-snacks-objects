76234825b889b458d1431597552cb3c0b0c66f84
DanielePilati <danielepdj@gmail.com>
2024-05-22 01:52:29 +0200

    [upd] add main snack 3

---
+++ snack-3/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Crea un array di oggetti che rappresentano degli animali.
+const animali = [
+  // Ogni animale ha un nome, una famiglia e una classe.
+  {
+    nome: "leone",
+    famiglia: "felidi",
+    classe: "mammiferi",
+  },
+  {
+    nome: "cane",
+    famiglia: "canidi",
+    classe: "mammiferi",
+  },
+  {
+    nome: "gallina",
+    famiglia: "fasianidi",
+    classe: "uccelli",
+  },
+];
+// Crea un nuovo array
+let mammiferi = [];
+//con la lista dei mammiferi.
+for (let i = 0; i < animali.length; i++) {
+  if (animali[i].classe === "mammiferi") {
+    mammiferi[i] = animali[i];
+  }
+}
+console.log(mammiferi);
