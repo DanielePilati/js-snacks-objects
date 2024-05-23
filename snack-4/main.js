0132804f9019144d881c435d68eba655379e0033
DanielePilati <danielepdj@gmail.com>
2024-05-23 20:12:55 +0200

    [upd] add map

--- snack-4/main.js
+++ snack-4/main.js
@@ -16,10 +16,17 @@ const persone = [
   },
 ];
 // Crea quindi un nuovo array
-let canDrive = [];
+let canDrive = persone.map((persona) => {
+  if (persona.eta >= 18) {
+    persona.testo = `hai ${persona.eta} anni e puoi guidare`;
+  } else {
+    persona.testo = `hai ${persona.eta} anni e NON puoi guidare`;
+  }
+  return { ...persona };
+});
 
 //inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
-
+/*
 for (let i = 0; i < persone.length; i++) {
   canDrive[i] = persone[i];
   if (canDrive[i].eta >= 18) {
@@ -27,6 +34,7 @@ for (let i = 0; i < persone.length; i++) {
   } else {
     canDrive[i].testo = `hai ${canDrive[i].eta} anni e NON puoi guidare`;
   }
-  delete canDrive[i].eta;
 }
+*/
+
 console.log(canDrive);
